05538e69cf433691a5eee87c9c287aa6be423fa7
DayseKelly <146476197+DayseKelly@users.noreply.github.com>
2024-11-22 18:57:19 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles.css">
+  <title>atv02-dayse-rodrigues-19-11</title>
+</head>
+<body>
+
+  <h1>FORMULÁRIO </h1>
+
+
+
+  <ul>
+    <div class="titulo">
+      <div id="div1">
+        <ul>
+          <li><a href="ceteia.guanambi.ifbaiano.edu.br/pswmedio/2023/2ai/buffet/src/index.php">buffet</a></li>
+      </div>
+      <div id="div1">
+        <ul>
+          <li><a href="ceteia.guanambi.ifbaiano.edu.br/pswmedio/2023/2ai/cineview/cineview/index.php">cineview</a></li>
+      </div>
+      <div id="div1">
+        <ul>
+          <li><a href="ceteia.guanambi.ifbaiano.edu.br/pswmedio/2023/2ai/livraria/">livraria</a></li>
+      </div>
+      <div id="div1">
+        <ul>
+          <li><a href="ceteia.guanambi.ifbaiano.edu.br/pswmedio/2023/2ai/neurobiblioteca/">neurobiblioteca</a></li>
+      </div>
+      <div id="div1">
+        <ul>
+          <li><a href="ceteia.guanambi.ifbaiano.edu.br/pswmedio/2023/2ai/sisdoacao/">doação</a></li>
+      </div>
+      <div id="div1">
+        <ul>
+          <li><a href="ceteia.guanambi.ifbaiano.edu.br/pswmedio/2023/2ai/sisgravadora/funcoes/login.php">gravadora</a>
+          </li>
+      </div>
+      <div id="div1">
+        <ul>
+          <li><a href="ceteia.guanambi.ifbaiano.edu.br/pswmedio/2023/2bi/sisesporte/esporte/">esporte</a></li>
+      </div>
+      <div id="div1">
+        <ul>
+          <li><a href="ceteia.guanambi.ifbaiano.edu.br/pswmedio/2023/2bi/sisrestaurante/">restaurante</a></li>
+      </div>
+
+      </li>
+    </div>
+
+
+
+
+    <div class="caixa de entrada">
+      <div id="div2">
+
+        <form>
+          <label for="fname">nome do usuário:</label>
+          <input type="text" id="fname" name="fname" placeholder="digite aqui"><br><br>
+          <label for="pwd">senha:</label>
+          <input type="password" id="pwd" name="pwd"  placeholder="digite aqui"><br><br>
+          <label for="lname">endereço:</label>
+          <input type="text" id="lname" name="lname" placeholder="digite aqui"><br><br>
+          <label for="birthday">data de nascimento</label>
+          <input type="date" id="data de nascimento" name="birtheday" placeholder="digite aqui"><br><br>
+          <label for="number">cep:</label>
+          <input type="number" id="cep" name="number" placeholder="000-000"><br><br>
+          <label for="fname">rua:</label>
+          <input type="text" id="fname" name="fname" placeholder="digite aqui"><br><br>
+          <label for="phone">seu número:</label>
+          <input type="tel" id="phone" name="phone" placeholder="(00)00000-0000"><br><br>
+          <label for="favcolor">Sua cor preferida:</label>
+          <input type="color" id="favcolor" name="favcolor"><br><br>
+          <label for="birthdaytime">data e hora da inscrição:</label>
+          <input type="datetime-local" id="birthdaytime" name="digite aqui"><br><br>
+          <label for="email">seu email:</label>
+          <input type="email" id="email" name="email" placeholder="digite aqui"><br><br>
+          <label for="myfile">foto do RG:</label>
+          <input type="file" id="myfile" name="digite aqui"><br><br>
+          <label for="quantity">fluente em quantas línguas?:</label>
+          <input type="number" id="quantity" name="quantity" placeholder="selecione" min="0" max="10"><br><br>
+          <label for="idad">idade:</label>
+          <input type="idad" id="idade" name="idad" placeholder="ex: 17"><br><br>
+          <p>relacionamento:</p>
+          <input type="radio" id="relacionamento" name="fav_language" value="HTML">
+          <label for="html">solteiro</label>
+          <input type="radio" id="relacionamento" name="fav_language" value="HTML">
+          <label for="html">casado</label>
+          <input type="radio" id="relacionamento" name="fav_language" value="HTML">
+          <label for="html">viuvo</label>
+          <input type="radio" id="relacionamento" name="fav_language" value="HTML">
+          <label for="html">namorando</label><br>
+          <p>genêro:</p>
+          <input type="checkbox" id="genêro" name="vehicle1" value="Bike">
+          <label for="vehicle1"> feminino</label>
+          <input type="checkbox" id="genêro" name="vehicle2" value="Car">
+          <label for="vehicle2"> masculino</label>
+          <input type="checkbox" id="genêro" name="vehicle3" value="Boat">
+          <label for="vehicle3"> prefiro não dizer</label><br><br>
+          <input type="button" onclick="alert('seja bem-vindo!')" value="Click  aqui">
+          <input type="submit" value="enviar">
+          <input type="reset" value="refazer">
+
+
+        </form>
+      </div>
+
+    
+    
+
+
+<br>
+<br>
+      
+        
+      
+
+
+
+
+
+    <div class="rodape">
+      <div id="div6"></div>
+      <div id="div7"> &copy; DIREITOS DE DAYSE KELLY RODRIGUES</div>
+      <div id="div9"></div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+
+</html>
